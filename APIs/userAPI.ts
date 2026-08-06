b9aaff7faf9fa285f9da2f09a01ec0f62579c07f
m-usman933 <rao.usman933@gmail.com>
2026-08-06 07:54:25 +0500

    Added APIs layer and moved all the APIs to the API layer making abstraction for fixtures

---
+++ APIs/userAPI.ts
@@ -0,0 +1,41 @@
+import { APIRequestContext } from "@playwright/test";
+import { UserData } from "../factories/userFactory";
+
+export class UserApi
+{
+    constructor(private request : APIRequestContext){};
+    
+    async createUser(userInfomation:UserData)
+    {
+         const response = await this.request.post
+        (
+            'https://demoqa.com/Account/v1/User',
+            {
+                data : userInfomation,
+            }
+        )
+        return response;
+    }
+
+    async generateToken(userInfomation : UserData)
+    {
+        const GenerateTokenResponse = await this.request.post(
+        "https://demoqa.com/Account/v1/GenerateToken",
+        {
+            data: userInfomation,
+        });
+        return GenerateTokenResponse;
+    }
+
+    async deleteUser(userId : number , token : any)
+    {
+        const deletionResponse = await this.request.delete(
+            `https://demoqa.com/Account/v1/User/${userId}`,
+                {
+                    headers:{
+                    Authorization : `Bearer ${token}`,
+                }
+            });
+        return deletionResponse;
+    }
+}
--- fixtures/newUserAPIFixture.ts
+++ fixtures/newUserAPIFixture.ts
@@ -1,61 +1,39 @@
 import {test as base , expect} from "@playwright/test";
 import { createUserData, UserData } from "../factories/userFactory";
+import { UserApi } from "../APIs/userAPI";
 
 export type MyFixtures ={
     testUser : UserData
 }
-
 export const test = base.extend<MyFixtures>({
     
     testUser: async ({ request }, use) => 
     {
+        const userApi = new UserApi(request);
         let userId : number;
         const userData = createUserData();
 
-        const response = await request.post(
-            'https://demoqa.com/Account/v1/User',
-            {
-                data : userData
-            }
-        )
-        const responseBody = await response.json();
-        userId = responseBody.userID;
+        const createUserResponse = await userApi.createUser(userData);
+        const registerResponseBody = await createUserResponse.json();
+        userId = registerResponseBody.userID;
         
-        expect(response.status()).toBe(201);
-        expect(responseBody.username === userData.userName).toBeTruthy();
+        expect(createUserResponse.status()).toBe(201);
+        expect(registerResponseBody.username === userData.userName).toBeTruthy();
 
-        const GenerateTokenResponse = await request.post(
-        "https://demoqa.com/Account/v1/GenerateToken",
-        {
-            data: {
-            userName: userData.userName,
-            password: userData.password,
-        }
-        }
-    );
+        const GenerateTokenResponse = await userApi.generateToken(userData);
+        const tokenResponseBody = await GenerateTokenResponse.json();
 
-        const body = await GenerateTokenResponse.json();
-        const token = body.token;
+        const token = tokenResponseBody.token;
 
         expect(GenerateTokenResponse.status()).toBe(200);
-        expect(body.status).toBe("Success");
-        expect(body.token).toBeTruthy();
+        expect(tokenResponseBody.status).toBe("Success");
+        expect(tokenResponseBody.token).toBeTruthy();
 
         await use(userData);
-
         if(userId)
         {
-
-            const deleteResponse = await request.delete(
-                `https://demoqa.com/Account/v1/User/${userId}`,
-                {
-                    headers:{
-                    Authorization : `Bearer ${token}`,
-                }
-                }
-                
-            )
-            expect(deleteResponse.status()).toBe(200);
+            const deleteResponse = await userApi.deleteUser(userId,token);
+            expect(deleteResponse.status()).toBe(204);
            
         }
     }
