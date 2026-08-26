e7b9a2a1150b0201824304524d87503d7000c7e7
m-usman933 <rao.usman933@gmail.com>
2026-08-26 05:40:14 +0500

    Adding environment file and added it to GIT Ignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,10 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+  import dotenv from 'dotenv';
+  import path from 'path';
+  dotenv.config(
+    { path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -25,7 +26,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'https://demoqa.com',
+    baseURL: process.env.BASE_URL,
     trace: 'on-first-retry',
   },
 
