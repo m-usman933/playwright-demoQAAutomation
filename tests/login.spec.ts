14829de61deb613819770ca133c2b604f151ca52
m-usman933 <rao.usman933@gmail.com>
2026-08-29 06:13:27 +0500

    Updated login as it was on Blank page earlier

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,10 +4,11 @@ import {LoginPage} from  "../pages/loginPage";
 
 test("Valid login to demoQA", async({loginPage, page})=>
 {
+    await page.goto('/profile');
     await expect(page).toHaveURL('https://demoqa.com/profile');
     await page.getByText('Elements').click();
     await page.locator('[href="/checkbox"]').click();
 
     await page.getByRole('checkbox',{name :'Select Home'}).check();
-    await page.waitForTimeout(5000);
+    //await page.waitForTimeout(5000);
 })
